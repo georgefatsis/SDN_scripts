c9ed5ceb97c9c6c2206864356b69407793c413af
georgefatsis <fats_george@msn.com>
2020-06-24 06:03:35 +0300

    Update run_CORAL.sh

--- run_CORAL.sh
+++ run_CORAL.sh
@@ -16,11 +16,11 @@
 Log "CORAL simple implementation is starting now"
 Log "cooja is starting"
 gnome-terminal -e "bash -c \" ~/SDN_scripts/startcooja.sh; exec bash\" "
-sleep 100;
-
-Log "Coral Adapter is starting"
-gnome-terminal -e "bash -c \" cd ~/coral-sdn-adapter-COOJA-runtime ; sudo ./coral-sdn-adapter; exec bash\" "
-sleep 100;
+sleep 60;
 
 Log "Starting CORAL Controller"
 gnome-terminal -e "bash -c \" cd ~/CORAL-SDN-Controller ; java -jar CoralSDNControler.jar; exec bash\" "
+sleep 60; 
+
+Log "Coral Adapter is starting"
+gnome-terminal -e "bash -c \" cd ~/coral-sdn-adapter-COOJA-runtime ; sudo ./coral-sdn-adapter; exec bash\" "
